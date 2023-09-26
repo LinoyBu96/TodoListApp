94d2587b082cfc9d2f285c43731418ddaa500983
LinoyBu96 <linoy.bsr@gmail.com>
2023-09-27 02:47:35 +0300

    create more general code

--- index.js
+++ index.js
@@ -8,110 +8,109 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var shoppingData = [{task: "task 1", note:"note 1", checked: ""}, {task: "task 2", note:"note 2", checked: "checked"}];
 
-function get_active_data(isActive) {
+var homeData = [{task: "task 1", note:"note 1", checked: ""}, {task: "task 2", note:"note 2", checked: "checked"}];
+var shoppingData = [{task: "task 3", note:"note 3", checked: ""}, {task: "task 4", note:"note 4", checked: "checked"}];
+var studyData = [{task: "task 5", note:"note 5", checked: ""}, {task: "task 6", note:"note 6", checked: "checked"}];
+var jobData = [{task: "task 7", note:"note 7", checked: ""}, {task: "task 8", note:"note 8", checked: "checked"}];
+
+
+function getData(taskType) {
   var home = "link-body-emphasis";
   var shopping = "link-body-emphasis";
   var study = "link-body-emphasis";
   var job = "link-body-emphasis";
+  var data = homeData;
 
-  switch (isActive) {
+  switch (taskType) {
     case 'home':
       home = activeStatus;
       break;
     case 'shopping':
       shopping = activeStatus;
+      data = shoppingData;
       break;
     case 'study':
       study = activeStatus;
+      data = studyData;
       break;
     case 'job':
       job = activeStatus;
+      data = jobData;
       break;
     default:
       console.log(`Unexpected error.`);
   }
-  
-  return {homeStatus: home, 
+  return {tasksData: data,
+    homeStatus: home, 
     shoppingStatus: shopping,
     studyStatus: study,
     jobStatus: job};
-  
-  // res.render(__dirname + "/views/index.ejs", data);
+}
+
+
+function getRequestHandler(taskType, res) {
+  const data = getData(taskType);
+  if (taskType === "home") {
+    taskType = "";
+  }
+  data.taskType = taskType;
+  res.render("index" + ".ejs", data);
+}
+
+function postRequestHandler(taskType, req, res, data) {
+  if (req.body["number"]) {
+    const i = req.body["number"];
+    const oldVal = data[i]["checked"];
+    var newVal = "";
+    if (oldVal == "") {
+      newVal = "checked"
+    }
+    data[i]["checked"] = newVal;
+  } else {
+    const newTask = {task: req.body["task"], note: req.body["note"], checked: ""};
+    data.push(newTask);
+  }
+  getRequestHandler(taskType, res);
 }
 
 app.get("/", (req, res) => {
-  const data = get_active_data("home");
-  res.render("home.ejs", data);
+  getRequestHandler("home", res);
 });
 
 app.get('/shopping', (req, res) => {
-  console.log(shoppingData[0].task);
-  const data = get_active_data("shopping");
-  data.shoppingData = shoppingData;
-  res.render("shopping.ejs", data);
+  getRequestHandler("shopping", res);
 });
 
 app.get('/study', (req, res) => {
-  const data = get_active_data("study");
-  res.render("study.ejs", data);
+  getRequestHandler("study", res);
 });
 
 app.get('/job', (req, res) => {
-  const data = get_active_data("job");
-  res.render("job.ejs", data);
+  getRequestHandler("job", res);
+});
+
+app.post('/', (req, res) => {
+  console.log("post-home");
+  postRequestHandler("home", req, res, homeData)
 });
 
 app.post('/shopping', (req, res) => {
   console.log("post-shopping");
-  const data = get_active_data("shopping");
-  // var userTask = req.body["task"];
-  // data.newTask = userTask;
-  const newTask = {task: req.body["task"], note: req.body["note"], checked: ""};
-  shoppingData.push(newTask);
-  data.shoppingData = shoppingData;
-  res.render("shopping.ejs", data);
+  postRequestHandler("shopping", req, res, shoppingData)
 });
 
-
-app.post('/shopping/checkbox', (req, res) => {
-  console.log("post-shopping/checkbox");
-  const i = req.body["number"];
-  const oldVal = shoppingData[i]["checked"];
-  var newVal = "";
-  if (oldVal == "") {
-    newVal = "checked"
-  }
-  shoppingData[i]["checked"] = newVal;
-  res.redirect("/shopping");
+app.post('/study', (req, res) => {
+  console.log("post-study");
+  postRequestHandler("study", req, res, studyData)
 });
 
+app.post('/job', (req, res) => {
+  console.log("post-job");
+  postRequestHandler("job", req, res, jobData)
+});
 
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-
-
-
-// const checkbox = $("[type=checkbox]")
-
-// checkbox.addEventListener('change', (event) => {
-//   if (event.currentTarget.checked) {
-//     alert('checked');
-//   } else {
-//     alert('not checked');
-//   }
-// })
-
-// function updateH1(checkbox) {
-//   // var h1 = document.getElementById('status');
-//   if (checkbox.checked) {
-//       console.log("on");
-//   } else {
-//     console.log("off");
-//   }
-// }
-
